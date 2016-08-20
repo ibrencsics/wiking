9d2203aa55b4b5ea968456f907e464ccd572b7ca
ibrencsics <ivan.brencsics@gmail.com>
2016-08-20 20:57:12 +0200

    Pattern matching used for regex

--- wikidump-parser/src/main/scala/extractor/WikiExtractor.scala
+++ wikidump-parser/src/main/scala/extractor/WikiExtractor.scala
@@ -14,7 +14,6 @@ object Line {
   }
 }
 
-//case class Template(name: String, data: List[String])
 case class ParsedTemplate(name: String, data: List[(String, List[Element])])
 
 sealed trait Element
@@ -35,19 +34,18 @@ object FreeText {
 
   type Free = List[Element]
 
-  val REGEXP_SEP = """^\s*(,)$|^(and)\s*$""".r
-  val REGEXP_OF = """^of\s*(the)*$""".r
-  val REGEXP_LF = """(&lt;br\s*/{0,1}&gt;)|(\\n)|(&lt;!--.*--&gt;)""".r // the last of is comment
-  val REGEXP_SMALL = """&lt;/?small\s*/{0,1}&gt;""".r
+  val R_SEP = """(^\s*,\s*$|^\s*and\s*$)""".r
+  val R_OF = """(^of\s*(the)*$)""".r
+  val R_LF = """(&lt;br\s*/{0,1}&gt;)|(\\n)|(&lt;!--.*--&gt;)""".r // the last of is comment
+  val R_SMALL = """&lt;/?small\s*/{0,1}&gt;""".r
 
   val R_YEAR = "(\\d{1,4})"
   val R_MONTH = "([Jj]anuary|[Ff]ebruary|[Mm]arch|[Aa]pril|[Mm]ay|[Jj]une|[Jj]uly|[Aa]ugust|[Ss]eptember|[Oo]ctober|[Nn]ovember|[Dd]ecember)"
-  val R_DAY = "(\\d{1,2}|\\d{1,2}\\sor\\s\\d{1,2})" //\\s+(or\\s+\\d{1,2}))"
+  val R_DAY = "(\\d{1,2}|\\d{1,2}\\sor\\s\\d{1,2})"
   val R_AD_BC = "(AD|BC)?"
-  val R_DATE = new Regex(R_DAY + "\\s" + R_MONTH + "\\s" + R_YEAR + "\\s?" + R_AD_BC)
-  val R_FROM_TO = new Regex(R_DATE.regex + "\\s*[–-]\\s*" + R_DATE.regex)
-
-  val R_FROM_TO_YEARS_ONLY = new Regex(R_YEAR + "\\s?" + "–" + R_YEAR + "\\s?" + R_AD_BC)
+  val R_DATE = new Regex(R_DAY + "\\s" + R_MONTH + "\\s" + R_YEAR + "\\s?" + R_AD_BC + ".*")
+  val R_FROM_TO = new Regex(R_DATE.regex + "\\s*[–-]\\s*" + R_DATE.regex + ".*")
+  val R_FROM_TO_YEARS_ONLY = new Regex(R_YEAR + "\\s?" + "–" + R_YEAR + "\\s?" + R_AD_BC + ".*")
 
   // http://stackoverflow.com/questions/15491894/regex-to-validate-date-format-dd-mm-yyyy
 
@@ -61,42 +59,16 @@ object FreeText {
       buf.clear()
 
       // eliminate line feed
-      val smallRemoved = REGEXP_SMALL.replaceAllIn(text, " ")
-      val trimmed = REGEXP_LF.replaceAllIn(smallRemoved, " ").trim
+      val smallRemoved = R_SMALL.replaceAllIn(text, " ")
+      val trimmed = R_LF.replaceAllIn(smallRemoved, " ").trim
 
-      if (REGEXP_SEP.findFirstIn(trimmed).nonEmpty) {
-        elems += Sep(trimmed)
-
-      } else if (REGEXP_OF.findFirstIn(trimmed).nonEmpty) {
-        elems += Of(trimmed)
-
-//      } else if (RegexHelper.Lf.findFirstIn(trimmed).nonEmpty) {
-//        elems += Lf(trimmed)
-
-      } else if (R_FROM_TO.findFirstIn(trimmed).nonEmpty) {
-        R_FROM_TO.findAllIn(trimmed).matchData foreach {
-          m => elems += Timeframe(
-            Date(m.group(1), m.group(2), m.group(3), isAd(m.group(4))),
-            Date(m.group(5), m.group(6), m.group(7), isAd(m.group(8))))
-        }
-
-      } else if (R_DATE.findFirstIn(trimmed).nonEmpty) {
-        R_DATE.findAllIn(trimmed).matchData foreach {
-          m => elems += Date(m.group(1), m.group(2), m.group(3), isAd(m.group(4)))
-        }
-
-      } else if (R_FROM_TO_YEARS_ONLY.findFirstIn(trimmed).nonEmpty) {
-        R_FROM_TO_YEARS_ONLY.findAllIn(trimmed).matchData foreach {
-          m => elems += Timeframe(Date("", "", m.group(1), isAd(m.group(3))), Date("", "", m.group(2), isAd(m.group(3))))
-        }
-
-//      } else if (new Regex(R_YEAR).findFirstIn(trimmed).nonEmpty) {
-//        new Regex(R_YEAR).findAllIn(trimmed).matchData foreach {
-//          m => elems += Date("", "", m.group(1), true)
-//        }
-
-      } else if (trimmed.size > 0 || trimmed == ' ') {
-        elems +=  Text(trimmed)
+      trimmed match {
+        case R_SEP(text) => elems += Sep(text)
+        case R_OF(text, _) => elems += Of(text)
+        case R_FROM_TO(d1, m1, y1, a1, d2, m2, y2, a2) => elems += Timeframe(Date(d1, m1, y1, isAd(a1)), Date(d2, m2, y2, isAd(a2)))
+        case R_DATE(d, m, q, a) => elems += Date(d, m, q, isAd(a))
+        case R_FROM_TO_YEARS_ONLY(y1, y2, a) => elems += Timeframe(Date("", "", y1, isAd(a)), Date("", "", y2, isAd(a)))
+        case text => if (text.size > 0 || text == " ") elems += Text(text)
       }
 
       def isAd(data: String): Boolean = data == null || (data != null && data == "AD")
@@ -151,9 +123,6 @@ object FreeText {
       else if (!linkPresent && !templatePresent && !c.equals('[') && !c.equals(']') && !c.equals('{') && !c.equals('}')) {
         buf += c.toString
       }
-
-
-
 
       pos += 1
     }
@@ -213,20 +182,3 @@ object WikiExtractor {
     r.findFirstMatchIn(raw).map(_.group(1)).map(i => if (i.isEmpty) 0 else Integer.valueOf(i))
   }
 }
-
-
-// succession -> 0  : [[Crown of Aragon|King of Aragon]]
-// reign -> 0       : 2 April 1416 – 27 June 1458
-// predecessor -> 0 : [[Ferdinand I of Aragon|Ferdinand I]]
-// successor -> 0   : [[John II of Aragon|John II]]
-// succession -> 1  : [[List of monarchs of Naples|King of Naples]] [[List of monarchs of Sicily|and Sicily]]
-// reign -> 1       : 2 June 1442 – 27 June 1458
-// predecessor -> 1 : [[René of Anjou|René]]
-// successor -> 1   : [[Ferdinand I of Naples|Ferdinand I]]
-
-
-// TODO
-// Link(Basileus,None), Of(of), Link(Macedon,None)
-// Text(, Macedon)
-// deathDate -> Text(27 June {{death year and age|df=yes|1458|1396}})
-// Text(&lt;small&gt;(illegitimate)&lt;/small&gt;)
